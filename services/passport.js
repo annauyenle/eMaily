3603826171a69d05c2ab22170d0f893b94f2117e
Anna Le <anna.le@lmi.com>
2021-11-01 18:17:16 -0400

    Created basic components and structure

--- services/passport.js
+++ services/passport.js
@@ -24,28 +24,13 @@ passport.use(
     callbackURL: '/auth/google/callback',
     proxy: true
   },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then(existingUser => {
-          if (existingUser) {
-            console.log('User already exists in database.')
-            done(null, existingUser)
-          } else {
-            new User({
-              googleId: profile.id,
-              displayName: profile.displayName
-            }).save((err, res) => {
-              if (err) {
-                console.log('Unable to insert new user instance. Error:', err)
-              } else {
-                console.log('Successfully inserted:', res)
-              }
-            }).then(newUser => {
-              done(null, newUser)
-            })
-          }
-        }).catch(err => {
-          console.log('Something went wrong with the OAuth process. Error:', err)
-        })
-    })
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id })
+
+    if (existingUser) {
+      return done(null, existingUser)
+    }
+    newUser = await new User({ googleId: profile.id, displayName: profile.displayName }).save()
+      done(null, newUser)
+  })
 )
