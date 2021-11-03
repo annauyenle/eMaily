df2ff222e031586d0c26cdcfae730d238c99733c
Anna Le <anna.le@lmi.com>
2021-11-02 21:09:29 -0400

    Login/Logout functionality added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react"
+import { connect } from 'react-redux'
 
 class Header extends Component {
+
+  renderContent() {
+    switch (this.props.auth) {
+      case null: return
+      case false: return <li><a href="/auth/google">Login With Google</a></li>
+      default: return <li><a href="/api/logout">Logout</a></li>
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">
-            eMaily
-          </a>
+          <a href="" className="left brand-logo">eMaily</a>
           <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +25,8 @@ class Header extends Component {
   }
 }
 
-export default Header
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header)
